2d0fabe3c3fe5bc8554afd426d40e520e08c1e88
Gagik-arch <gagik.chilingaryan2020@gmail.com>
2022-08-13 14:15:50 +0400

    Changed days key in Calendar class.

--- calendar.js
+++ calendar.js
@@ -4,7 +4,7 @@ class Calendar {
 
     constructor(currentDate = new Date()) {
         this.currentDate = new Date(currentDate.setHours(0, 0, 0, 0))
-        this.data = this.initCalendar(this.currentDate)
+        this.days = this.initCalendar(this.currentDate)
     }
 
     initCalendar() {
@@ -14,12 +14,12 @@ class Calendar {
         const _firstDayOfWeek = this.#getFirstDayOfWeek(month + 1, year)
         const _currentMonthDayCount = this.#daysInMonth(month + 1, year)
         const _prevMonthDayCount = this.#daysInMonth(month, year)
-        const days = []
-        // console.log(this)
+        const array = []
+        console.log(this)
         if (_firstDayOfWeek > 0) {
             let p = _prevMonthDayCount - _firstDayOfWeek + 1, c = 1;
             while (p <= _prevMonthDayCount) {
-                days.push(
+                array.push(
                     new Day(
                         new Date(year, month - 1, p),
                         p,
@@ -29,7 +29,7 @@ class Calendar {
                 p++
             }
             while (c <= _currentMonthDayCount) {
-                days.push(new Day(
+                array.push(new Day(
                     new Date(year, month, c),
                     c,
                     this.#compareTwoDates(this.currentDate, new Date(year, month, c)) ? 'selected-day' : 'current-month'
@@ -39,7 +39,7 @@ class Calendar {
         } else {
             let i = 1
             while (i <= _currentMonthDayCount) {
-                days.push(new Day(
+                array.push(new Day(
                     new Date(year, month, i),
                     i,
                     this.#compareTwoDates(this.currentDate, new Date(year, month, i)) ? 'selected-day' : 'current-month'
@@ -48,49 +48,49 @@ class Calendar {
             }
         }
 
-        const remainder = days.length % this.weekDays.length
+        const remainder = array.length % this.weekDays.length
         let i = 1, end;
 
-        if (days.length <= 35) { // 7 : 5 block
-            end = 35 - days.length
+        if (array.length <= 35) { // 7 : 5 block
+            end = 35 - array.length
         }
         if (remainder) {
             end = this.weekDays.length - remainder
         }
         while (i <= end) {
-            days.push(new Day(new Date(year, month + 1, i), i, 'next-month'))
+            array.push(new Day(new Date(year, month + 1, i), i, 'next-month'))
             i++
         }
-        return days
+        return array
     }
 
     toDate(date) {
         this.currentDate = date
-        this.data = this.initCalendar(this.currentDate)
+        this.days = this.initCalendar(this.currentDate)
         return this
     }
 
     toNextMonth() {
         this.currentDate = this.#getNextMonth(this.currentDate)
-        this.data = this.initCalendar(this.currentDate)
+        this.days = this.initCalendar(this.currentDate)
         return this
     }
 
     toPrevMonth() {
         this.currentDate = this.#getPrevMonth(this.currentDate)
-        this.data = this.initCalendar(this.currentDate)
+        this.days = this.initCalendar(this.currentDate)
         return this
     }
 
     toNextYear() {
         this.currentDate = this.#getNextYear(this.currentDate)
-        this.data = this.initCalendar(this.currentDate)
+        this.ddaysys = this.initCalendar(this.currentDate)
         return this
     }
 
     toPrevYear() {
         this.currentDate = this.#getPrevYear(this.currentDate)
-        this.data = this.initCalendar(this.currentDate)
+        this.days = this.initCalendar(this.currentDate)
         return this
     }
 
